3b4ea2be1c6d090972fc57cd96d708f4cd94d2db
icdrt <info@icdrt.com>
2015-07-26 23:23:52 -0300

    new

--- descripcion_evento.php
+++ descripcion_evento.php
@@ -2,11 +2,11 @@
 
     include 'config.php'; //incluimos nuestro archivo config
 
-    $id=$_GET['id'];											// Aqui obtenemos el id del evento
-    $de=$conexion->query("SELECT * FROM eventos WHERE id=$id"); // y lo buscames en la base de dato
-    $row=$de->fetch_assoc();									// para buscar  y mostrar su
-    $titulo=$row['title'];									    // titulo y
-    $evento=$row['body'];									    // cuerpo
+    $id=$_GET['id'];						    // Aqui obtenemos el id del evento
+    $de=$conexion->query("SELECT * FROM eventos WHERE id=$id");     // y lo buscames en la base de dato
+    $row=$de->fetch_assoc();					    // para buscar  y mostrar su
+    $titulo=$row['title'];					    // titulo y
+    $evento=$row['body'];					    // cuerpo
 
 
  ?>
